2ccdd3a1f499bcf56090aae8129721309f014f76
Raneem <aljamal.raneem@gmail.com>
2023-12-12 12:54:53 +0200

    eight commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
-"use client"
-import { Element } from "react-scroll";
+"use client";
 import Home from "@/component/Home";
 import About from "@/component/About";
 import Experience from "@/component/Experience.jsx";
@@ -7,35 +6,22 @@ import Skills from "@/component/Skills.jsx";
 import ContactUs from "@/component/ContactUs.jsx";
 import ProjectsSection from "@/component/ProjectsSection";
 
-
-
-
 export default function HomePage() {
-
-  
   return (
     <>
-    <main>
-        <Element name="home">
-          <Home />
-        </Element>
-        <Element name="about">
-          <About id="about" />
-        </Element>
-        <Element name="experience">
-          <Experience id="experience" />
-        </Element>
-        <Element name="skills">
-          <Skills id="skills" />
-        </Element>
-        <Element name="projects">
-          <ProjectsSection id="projects" />
-        </Element>
-        <Element name="contact">
-          <ContactUs id="contact" />
-        </Element>
-      </main>
-   </>
-  )
-}
+      <main>
+        <Home />
 
+        <About id="about" />
+
+        <Experience id="experience" />
+
+        <Skills id="skills" />
+
+        <ProjectsSection id="projects" />
+
+        <ContactUs id="contact" />
+      </main>
+    </>
+  );
+}
